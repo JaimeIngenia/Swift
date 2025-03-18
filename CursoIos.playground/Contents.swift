5130cdcb90718445ed49f5e6838e0d278b6b037c
JaimeIngenia <jamoncayop@unal.edu.co>
2025-03-18 16:07:04 -0500

    Segundo dia

--- CursoIos.playground/Contents.swift
+++ CursoIos.playground/Contents.swift
@@ -9,7 +9,60 @@ let fixedGreeting = "Hello, world!"
 // CHAR Asignaci[on de un solo caracter
 var symbol = "!"
 
-// Subiendo a GitHub
+// Tipados
+
+var variableString:String = "Bimbo"
+
+var year:Int = 2025
+
+var altura:Float = 1.445
+
+var pi:Double = 3.14159
+
+// Operadores aritmeticos
+
+var a:Int = 5
+var b:Int = 10
+
+var sum = a + b
+var resta = a - b
+var multi = a * b
+var divide = a / b
+var module = a % b
+
+// Operadores de asignacin
+
+var example = 5
+example += 10
+example -= 10
+example *= 10
+example /= 10
+example %= 10
+
+// Operadores logicos comparacion
+
+let age2 = 30
+
+let isOlder = age2 >= 18
+let isYounger = age2 <= 18
+let isEqual = age2 == 18
+let isNotEqual = age2 != 18
+let isOrderOrEqual = age2 >= 18
+
+let isSunny:Bool = true
+let temperature:Int = 25
+
+let vamosAlParque:Bool = temperature > 25 && isSunny
+let goToBeach:Bool = temperature > 25 || isSunny
+let wearHat:Bool = !isSunny
+
+// Conversiones
+
+let integerNumber:Int = 10
+let decimalNumber:Double = 25.65
+let superNumber = Double(integerNumber) + decimalNumber
+
+
 
 
 
