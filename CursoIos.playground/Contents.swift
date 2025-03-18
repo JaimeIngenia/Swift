719aa210abd48ca5949a2547ace927f56113a20b
JaimeIngenia <jamoncayop@unal.edu.co>
2025-03-17 21:26:49 -0500

    Primer Commit

---
+++ CursoIos.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// Asignacion variable
+var greeting = "Hello, playground"
+
+// Asignacion fija
+let fixedGreeting = "Hello, world!"
+
+// CHAR Asignaci[on de un solo caracter
+var symbol = "!"
+
+// Subiendo a GitHub
+
+
+
+
