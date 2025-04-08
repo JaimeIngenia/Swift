3a55eacf9434b56d9d50645bfdc6863a70c43a99
JaimeIngenia <jamoncayop@unal.edu.co>
2025-04-07 21:30:16 -0500

    14vo commit

--- CursoIOSV2/CursoIOSV2/Components/MapExample.swift
+++ CursoIOSV2/CursoIOSV2/Components/MapExample.swift
@@ -21,32 +21,70 @@ struct MapExample: View {
     var body: some View {
         
         ZStack{
-            Map(position: $position)
+            MapReader{ proxy in
+                
+                
+                Map(position: $position){
+/*                    Marker("Pollito frito" , coordinate: CLLocationCoordinate2D(latitude: 1.2151865, longitude: -77.2750943))
+ */
+                    Annotation("Pollito Frito", coordinate: CLLocationCoordinate2D(latitude: 1.2151865, longitude: -77.2750943)){
+                        Circle().frame(height: 100)
+                    }.annotationTitles(.visible)
+                }
                 .mapStyle(.hybrid(elevation: .realistic, showsTraffic: true))
+//                .onMapCameraChange { contex in
+//                    print("Estamos en: \(contex.region)")
+//                }
+                .onMapCameraChange(frequency: .continuous) { contex in
+                    print("Estamos en: \(contex.region)")
+                }
+                .onTapGesture { coord in
+                    if let coordinates = proxy.convert(coord, from: .local){
+                        withAnimation{
+                            
+                            position = MapCameraPosition.region(
+                                MKCoordinateRegion(
+                                    // center: CLLocationCoordinate2D(latitude: -77.2750943, longitude: 1.2151865),
+                                    center: CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude),
+                                    span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
+                                )
+                            )
+    
+                        }
+                    }
+                }
+        }
             VStack{
                 Spacer()
                 HStack{
                     Button("Ir al sur"){
-                        position = MapCameraPosition.region(
-                            MKCoordinateRegion(
-                                // center: CLLocationCoordinate2D(latitude: -77.2750943, longitude: 1.2151865),
-                                center: CLLocationCoordinate2D(latitude: 1.2151865, longitude: -77.2750943),
-                                span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                        withAnimation{
+                            
+                            position = MapCameraPosition.region(
+                                MKCoordinateRegion(
+                                    // center: CLLocationCoordinate2D(latitude: -77.2750943, longitude: 1.2151865),
+                                    center: CLLocationCoordinate2D(latitude: 1.2151865, longitude: -77.2750943),
+                                    span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
+                                )
                             )
-                        )
+    
+                        }
                     }
                     .padding(32)
                     .background(.white)
                     .padding()
                     
                     Button("Ir al norte"){
-                        position = MapCameraPosition.region(
-                            MKCoordinateRegion(
-                                // center: CLLocationCoordinate2D(latitude: -77.2750943, longitude: 1.2151865),
-                                center: CLLocationCoordinate2D(latitude: 1.2151865, longitude: -77.2750943),
-                                span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                        withAnimation{
+                            
+                            position = MapCameraPosition.region(
+                                MKCoordinateRegion(
+                                    // center: CLLocationCoordinate2D(latitude: -77.2750943, longitude: 1.2151865),
+                                    center: CLLocationCoordinate2D(latitude: 1.207094, longitude: -77.2789765),
+                                    span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)
+                                )
                             )
-                        )
+                        }
                     }
                     .padding(32)
                     .background(.white)
